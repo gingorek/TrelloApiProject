34a02f5c629d3abf40eb01513ea7c08ea1537f88
ignacylozinski <ignacy.lozinski@gmail.com>
2018-08-05 10:24:35 +0200

    Initialize Spring

---
+++ src/main/java/com/mycrud/trelloapiproject/TrelloapiprojectApplication.java
@@ -0,0 +1,12 @@
+package com.mycrud.trelloapiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloapiprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrelloapiprojectApplication.class, args);
+	}
+}
