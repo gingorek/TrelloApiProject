8415d9b158c3c82af2e8e17c3662baa312998048
ignacylozinski <ignacy.lozinski@gmail.com>
2018-08-07 11:20:14 +0200

    fixed main method

--- src/main/java/com/mycrud/trelloapiproject/TrelloapiprojectApplication.java
+++ src/main/java/com/mycrud/trelloapiproject/TrelloapiprojectApplication.java
@@ -1,22 +1,14 @@
 package com.mycrud.trelloapiproject;
 
-import com.mycrud.trelloapiproject.domain.TaskDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class TrelloapiprojectApplication  {
+public class TrelloapiprojectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TrelloapiprojectApplication.class, args);
+        public static void main(String[] args) {
+            SpringApplication.run(TrelloapiprojectApplication.class, args);
 
-	}
-
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
-//		return application.sources(TrelloapiprojectApplication.class);
-//
-//	}
+        }
 }
--- src/main/java/com/mycrud/trelloapiproject/controller/TrelloController.java
+++ src/main/java/com/mycrud/trelloapiproject/controller/TrelloController.java
@@ -1,13 +1,17 @@
 package com.mycrud.trelloapiproject.controller;
 
 import com.mycrud.trelloapiproject.client.TrelloClient;
+import com.mycrud.trelloapiproject.domain.CreatedTrelloCard;
 import com.mycrud.trelloapiproject.domain.TrelloBoardDto;
+import com.mycrud.trelloapiproject.domain.TrelloCardDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("v1/trello")
@@ -23,6 +27,13 @@ public class TrelloController {
 
         trelloBoards.forEach(trelloBoardDto -> {
             System.out.println(trelloBoardDto.getName() + " - " + trelloBoardDto.getId());
+            System.out.println("This board contains lists: ");
+            trelloBoardDto.getLists().forEach(trelloList ->
+                    System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
+        });
+
+/*        trelloBoards.forEach(trelloBoardDto -> {
+            System.out.println(trelloBoardDto.getName() + " - " + trelloBoardDto.getId());
 
             System.out.println("This board contain lists: ");
 
@@ -30,5 +41,13 @@ public class TrelloController {
                     System.out.println(trelloListDto.getName() + " - " + trelloListDto.getId()
                             + " - " + trelloListDto.isClosed()));
         });
+*/
     }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createTrelloCard")
+    public CreatedTrelloCard createdTrelloCard(@RequestBody TrelloCardDto trelloCardDto){
+        return trelloClient.createNewCard(trelloCardDto);
+    }
+
+    //id of list to test trello Api = "5b695cd2ec684423c61f3561"
 }
